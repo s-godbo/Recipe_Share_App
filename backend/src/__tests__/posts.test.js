1c24315b85c373d5482733cb45281db94c164bdc
s-godbo <samantha.godbold@bsu.edu>
2025-10-07 16:39:20 +0000

    chore: additional work on backend and frontend and updated tests

--- backend/src/__tests__/posts.test.js
+++ backend/src/__tests__/posts.test.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-import { describe, expect, test, beforeEach } from '@jest/globals'
+import { describe, expect, test, beforeEach, beforeAll } from '@jest/globals'
 import {
   createPost,
   listAllPosts,
@@ -10,16 +10,25 @@ import {
   deletePost,
 } from '../services/posts.js'
 import { Post } from '../db/models/post.js'
-const samplePosts = [
-  { title: 'Learning Redux', author: 'Daniel Bugl', tags: ['redux'] },
-  { title: 'Learn React Hooks', author: 'Daniel Bugl', tags: ['react'] },
-  {
-    title: 'Full-Stack React Projects',
-    author: 'Daniel Bugl',
-    tags: ['react', 'nodejs'],
-  },
-  { title: 'Guide to TypeScript' },
-]
+
+import { createUser } from '../services/users.js'
+
+let testUser = null
+let samplePosts = []
+
+beforeAll(async () => {
+  testUser = await createUser({ username: 'sample', password: 'user' })
+  samplePosts = [
+    { title: 'Learning Redux', author: testUser._id, tags: ['redux'] },
+    { title: 'Learn React Hooks', author: testUser._id, tags: ['react'] },
+    {
+      title: 'Full-Stack React Projects',
+      author: testUser._id,
+      tags: ['react', 'nodejs'],
+    },
+  ]
+})
+
 let createdSamplePosts = []
 beforeEach(async () => {
   await Post.deleteMany({})
@@ -29,6 +38,7 @@ beforeEach(async () => {
     createdSamplePosts.push(await createdPost.save())
   }
 })
+
 describe('getting a post', () => {
   test('should return the full post', async () => {
     const post = await getPostById(createdSamplePosts[0]._id)
@@ -41,22 +51,22 @@ describe('getting a post', () => {
 })
 describe('updating posts', () => {
   test('should update the specified property', async () => {
-    await updatePost(createdSamplePosts[0]._id, {
-      author: 'Test Author',
+    await updatePost(testUser._id, createdSamplePosts[0]._id, {
+      contents: 'some content change',
     })
     const updatedPost = await Post.findById(createdSamplePosts[0]._id)
-    expect(updatedPost.author).toEqual('Test Author')
+    expect(updatedPost.contents).toEqual('some content change')
   })
   test('should not update other properties', async () => {
-    await updatePost(createdSamplePosts[0]._id, {
-      author: 'Test Author',
+    await updatePost(testUser._id, createdSamplePosts[0]._id, {
+      contents: 'some content change',
     })
     const updatedPost = await Post.findById(createdSamplePosts[0]._id)
     expect(updatedPost.title).toEqual('Learning Redux')
   })
   test('should update the updatedAt timestamp', async () => {
-    await updatePost(createdSamplePosts[0]._id, {
-      author: 'Test Author',
+    await updatePost(testUser._id, createdSamplePosts[0]._id, {
+      contents: 'some content change',
     })
     const updatedPost = await Post.findById(createdSamplePosts[0]._id)
     expect(updatedPost.updatedAt.getTime()).toBeGreaterThan(
@@ -64,15 +74,15 @@ describe('updating posts', () => {
     )
   })
   test('should fail if the id does not exist', async () => {
-    const post = await updatePost('000000000000000000000000', {
-      author: 'Test Author',
+    const post = await updatePost(testUser._id, '000000000000000000000000', {
+      contents: 'some content change',
     })
     expect(post).toEqual(null)
   })
 })
 describe('deleting posts', () => {
   test('should remove the post from the database', async () => {
-    const result = await deletePost(createdSamplePosts[0]._id)
+    const result = await deletePost(testUser._id, createdSamplePosts[0]._id)
     expect(result.deletedCount).toEqual(1)
     const deletedPost = await Post.findById(createdSamplePosts[0]._id)
     expect(deletedPost).toEqual(null)
@@ -82,6 +92,7 @@ describe('deleting posts', () => {
     expect(result.deletedCount).toEqual(0)
   })
 })
+
 describe('listing posts', () => {
   test('should return all posts', async () => {
     const posts = await listAllPosts()
@@ -109,7 +120,7 @@ describe('listing posts', () => {
     )
   })
   test('should be able to filter posts by author', async () => {
-    const posts = await listPostsByAuthor('Daniel Bugl')
+    const posts = await listPostsByAuthor(testUser.username)
     expect(posts.length).toBe(3)
   })
   test('should be able to filter posts by tag', async () => {
@@ -117,15 +128,15 @@ describe('listing posts', () => {
     expect(posts.length).toBe(1)
   })
 })
+
 describe('creating posts', () => {
   test('with all parameters should succeed', async () => {
     const post = {
       title: 'Hello Mongoose!',
-      author: 'Huseyin Ergin',
       contents: 'This post is stored in a MongoDB database using Mongoose.',
       tags: ['mongoose', 'mongodb'],
     }
-    const createdPost = await createPost(post)
+    const createdPost = await createPost(testUser._id, post)
     expect(createdPost._id).toBeInstanceOf(mongoose.Types.ObjectId)
     const foundPost = await Post.findById(createdPost._id)
     expect(foundPost).toEqual(expect.objectContaining(post))
@@ -134,12 +145,11 @@ describe('creating posts', () => {
   })
   test('without title should fail', async () => {
     const post = {
-      author: 'Huseyin Ergin',
       contents: 'Post with no title',
       tags: ['empty'],
     }
     try {
-      await createPost(post)
+      await createPost(testUser._id, post)
     } catch (err) {
       expect(err).toBeInstanceOf(mongoose.Error.ValidationError)
       expect(err.message).toContain('`title` is required')
@@ -149,7 +159,7 @@ describe('creating posts', () => {
     const post = {
       title: 'Only a title',
     }
-    const createdPost = await createPost(post)
+    const createdPost = await createPost(testUser._id, post)
     expect(createdPost._id).toBeInstanceOf(mongoose.Types.ObjectId)
   })
 })
---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom'
+import { jwtDecode } from 'jwt-decode'
+import { User } from './User.jsx'
+import { useAuth } from '../contexts/AuthContext.jsx'
+
+export function Header() {
+  const [token, setToken] = useAuth()
+
+  if (token) {
+    const { sub } = jwtDecode(token)
+    return (
+      <div>
+        <h1>Welcome to the Recipe Exchange!</h1>
+        Logged in as <User id={sub} />
+        <br />
+        <button onClick={() => setToken(null)}>Logout</button>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h1>Welcome to the Recipe Exchange!</h1>
+      <br />
+      <Link to='/login'>Log In</Link> | <Link to='/signup'>Sign Up</Link>
+    </div>
+  )
+}
